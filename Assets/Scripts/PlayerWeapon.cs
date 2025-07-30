a76427915beb16474b1b2eef2e452e9fea401bc8
Javier Rojas <javierito.rojas.ramirez@gmail.com>
2025-07-30 15:52:49 +0100

    Player collision added  (CollisionHandler.cs)

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -5,6 +5,8 @@ public class PlayerWeapon : MonoBehaviour
 {
     [SerializeField] GameObject[] lasers;
     [SerializeField] RectTransform crosshair;
+    [SerializeField] Transform targetPoint;
+    [SerializeField] float targetDistance;
     
     bool _isFiring;
 
@@ -17,6 +19,8 @@ public class PlayerWeapon : MonoBehaviour
     {
         ProcessFiring();
         MoveCrosshair();
+        MoveTargetPoint();
+        AimLasers();
     }
 
     public void OnFire(InputValue value)
@@ -36,5 +40,21 @@ public class PlayerWeapon : MonoBehaviour
     void MoveCrosshair()
     {
         crosshair.position = Input.mousePosition;
+    }
+
+    void MoveTargetPoint()
+    {
+        Vector3 targetPointPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, targetDistance);
+        targetPoint.position = Camera.main.ScreenToWorldPoint(targetPointPosition);
+    }
+
+    void AimLasers()
+    {
+        foreach (GameObject laser in lasers)
+        {
+            Vector3 fireDirection = targetPoint.position - transform.position;
+            Quaternion rotationToTarget = Quaternion.LookRotation(fireDirection);
+            laser.transform.rotation = rotationToTarget;
+        }
     }
 }
